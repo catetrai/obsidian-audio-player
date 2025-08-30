4dd41aeeba8e4d8810aacb4fbac6847aba31f08c
Caterina Trainito <caterina.trainito@gmail.com>
2025-08-30 18:40:25 +0200

    Do not use named capture groups for compatibility with older JS version

--- src/utils.ts
+++ src/utils.ts
@@ -59,16 +59,13 @@ export function parseLrc(lrc: string): Array<Array<string>> {
 	 	Output is list of tuples [time, text]
 	*/
     const lrcEntries = [] as Array<Array<string>>;
-	type RegexGroups = {
-		time: string
-		text: string
-	}
-	
+
     lrc.split('\n').forEach(line => {
-        const match = line.match(/^\[(?<time>\d{2}:\d{2}(.\d{2})?)\](?<text>.*)/);
+        const match = line.match(/^\[(\d{2}:\d{2}(?:.\d{2})?)\](.*)/);
         if (match == null) return;
-        const res: Partial<RegexGroups> | undefined = match.groups;
-        lrcEntries.push([ parseTime(res?.time || ''), (res?.text || '').trim() ]);
+        const time = match.length > 1 ? match[1] : '';
+        const text = match.length > 2 ? match[2] : '';
+        lrcEntries.push([ parseTime(time), text.trim() ]);
     });
 
     function parseTime(time: string): string{
@@ -102,10 +99,6 @@ export function parseSrt(srt: string): Array<Array<string>> {
 		Output is list of tuples [startTime, endTime, text]
 	 */
     const srtEntries = [] as Array<Array<string>>;
-	type RegexGroups = {
-		startTime: string
-		endTime: string
-	}
 	
     srt.trim().split("\n\n").forEach(entry => {
 		let sections = entry.split('\n');
@@ -113,12 +106,13 @@ export function parseSrt(srt: string): Array<Array<string>> {
 		if (sections.length < 2) return;
 		const timeStr = sections[0];
         const match = timeStr.match(
-			/^(?<startTime>\d{2}:\d{2}:\d{2}([,.]\d{3})?) --> (?<endTime>\d{2}:\d{2}:\d{2}([,.]\d{3})?)/
+			/^(\d{2}:\d{2}:\d{2}(?:[,.]\d{3})?) --> (\d{2}:\d{2}:\d{2}(?:[,.]\d{3})?)/
 		);
 		if (match == null) return;
-        const res: Partial<RegexGroups> | undefined = match.groups;
+        const startTime = match.length > 1 ? match[1] : '';
+        const endTime = match.length > 2 ? match[2] : '';
 		const text = sections.slice(1).join('\n');
-        srtEntries.push([ parseTime(res?.startTime || ''), parseTime(res?.endTime || ''), text.trim() ]);
+        srtEntries.push([ parseTime(startTime), parseTime(endTime), text.trim() ]);
     });
 
     function parseTime(time: string): string{
